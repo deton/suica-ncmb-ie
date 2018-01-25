4da5261795a2d8c1f63c3e45e204042ee591bd56
KIHARA Hideto <deton@m1.interq.or.jp>
2018-01-25 21:10:01 +0900

    差額精算等のページにも対応するためウィンドウタイトルのマッチを修整

--- ncmb2ie/ie.ps1
+++ ncmb2ie/ie.ps1
@@ -1,4 +1,4 @@
 $total = &"node" ncmbtransit.js
 $shell = New-Object -ComObject Shell.Application
-$ie = ($shell.Windows() | ?{$_.name -eq 'Internet Explorer' -and $_.LocationName -eq 'マイオフィス (国内出張旅費後精算)'})
+$ie = ($shell.Windows() | ?{$_.name -eq 'Internet Explorer' -and $_.LocationName -like 'マイオフィス (国内出張*精算)'})
 $ie.Document.frames['bodys'].Document.forms[0].elements[21].value="$total"
